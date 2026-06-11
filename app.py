ba1ca21f7515268874fe496ed3e6f15c87565e45
leaf700 <kashing.yang@education.nsw.gov.au>
2026-06-11 03:05:36 +0000

    feature: add Bootstrap navbar partial with active states

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
